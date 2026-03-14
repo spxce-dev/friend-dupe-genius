0b66bae3a957e247dfaeae91bf3292e795ef67a0
spxce-dev <bmogolane3@gmail.com>
2026-03-14 20:44:46 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 function App() {
@@ -7,7 +10,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Index />} />
-        {/* Add other routes here if needed */}
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        {/* ADD ALL YOUR CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
